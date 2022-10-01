dbbc2de4284be29030de3c69ea1f2db8b63cebd9
ChristopherBoone98 <86747093+ChristopherBoone98@users.noreply.github.com>
2022-09-30 19:57:25 -0500

    Add files via upload

--- NASCAR stats from 1975 - 2003.sql
+++ NASCAR stats from 1975 - 2003.sql
@@ -1,199 +1,157 @@
-SELECT driver, COUNT(yearRace) as NumOfRaces
-FROM nascard
-WHERE year = '1998'
-GROUP BY driver
-ORDER BY NumOfRaces DESC
+-- In this project, I will be looking at 2 nascar datasets, "nascarr" and "nascard".
+-- "nascarr" goes over the race results that contains 17 columns
+-- "nascard" goes over the driver results that contains 10 columns
+-- Both of these datasets are similar because it goes over the seasons between 1975 and 2003.
+-- In this project, I will be focusing on the 1992 season.
 
--- The dataset
 Select *
-From nascard
+From nascarr
+
+SELECT *
+FROM nascard
+Order by 1,2
+
+-- 1992 season
+
+SELECT *
+FROM nascard
+WHERE year = '1992'
+ORDER by 1,2
 
 SELECT *
 FROM nascarr
+Where year = '1992'
+Order by 1,2
 
--- Most wins from 1975 - 2003 (Top 10)
+-- Race example: Daytona in serRace 506
 
-SELECT TOP 10 driver, COUNT(finish) AS Wins
+SELECT *
+FROM nascarr
+WHERE year = '1992' and serRace = 506
+ORDER by 1,2
+
+SELECT *
 FROM nascard
-WHERE finish = 1
-GROUP BY driver
-ORDER BY Wins DESC
+Where year = '1992' and serRace = 506
+Order by 1,2
 
--- Most manufacturer wins from 1975 - 2003
+-- Number of races in 1992
 
-SELECT carMake, COUNT(finish) AS Wins
+SELECT COUNT(cast(yrRacr as int)) as NumOfRaces
+FROM nascarr
+WHERE year = '1992'
+
+-- Track Types: Ovals
+
+Select track, roadTrk
+From nascarr
+Where roadTrk = 0 and year = '1992'
+Group by track, roadTrk
+
+-- Track Types: Road Course
+
+Select track, roadTrk
+From nascarr
+Where roadTrk = 1 and year = '1992'
+Group by track, roadTrk
+
+-- Manufacturers in 1992
+
+SELECT carMake 
 FROM nascard
-WHERE finish = 1
-GROUP BY carMake
-ORDER BY Wins DESC
+Where year = '1992' 
+Group by carMake
 
--- Most Ford wins by a driver from 1975 - 2003
+-- Most races by a driver in 1992
 
-SELECT carMake, driver, COUNT(finish) AS Wins
+Select driver, COUNT(yearRace) as NumOfRaces
 FROM nascard
-WHERE finish = 1 AND carMake = 'Ford'
-GROUP BY carMake, driver
-ORDER BY Wins DESC
+Where year = '1992'
+Group by driver
+order by NumOfRaces Desc
 
--- Create Joins
+-- Most wins by a driver in 1992
+
+Select driver, COUNT(finish) as Wins
+FROM nascard
+Where finish = 1 AND year = '1992'
+Group by driver
+order by Wins Desc
+
+-- Most Top 5s in 1992
+
+Select TOP 10 driver, COUNT(finish) as Top5s
+FROM nascard
+Where finish <= 5 AND year = '1992'
+Group by driver
+order by Top5s Desc
+
+-- Most Top 10s in 1992
+
+Select TOP 10 driver, COUNT(finish) as Top10s
+FROM nascard
+Where finish <= 10 AND year = '1992'
+Group by driver
+order by Top10s Desc
+
+-- Best Average finish in 1992 for drivers who ran 22 or more races
+
+Select driver, COUNT(yearRace) as NumOfRaces,  ROUND(AVG(cast(finish as float)),1) as AvgFinish
+FROM nascard
+Group by driver, year
+HAVING COUNT(yearRace) >= 22 AND year = '1992'
+order by AvgFinish
+
+-- Most Earnings by a driver in 1992
+
+Select TOP 10 driver, Sum(cast(prize as int)) as TotalEarnings
+FROM nascard
+Where year = '1992'
+Group by driver
+Order by TotalEarnings desc
+
+-- Most wins by Manufacturer
+
+Select carMake as Manufacturer, COUNT(finish) as Wins
+From nascard
+Where year = '1992' and finish = 1
+Group by carMake
+Order by Wins desc
+
+-- Creating Joins using both datasets
 
 Select race.serRace, race.year, yrRacr, numCars, race.prize AS prize,
 race.laps, roadTrk, track, driver AS RaceWinner, carMake, driver.prize AS WinningPrize
 FROM nascarr AS race
 JOIN nascard AS driver
    ON race.serRace = driver.serRace
-WHERE finish = 1
+WHERE finish = 1 and race.year = '1992'
 
--- Most Talladega wins by a driver from 1975 - 2003
+-- Joins with case statements
 
-Select track, driver, COUNT(finish) as Wins
-FROM nascarr AS race
-JOIN nascard AS driver
-   ON race.serRace = driver.serRace
-WHERE finish = 1 and track LIKE 'T%a%l%l%a%'
-GROUP BY track, driver
-ORDER BY Wins DESC
-
--- Most road course wins
-
-Select roadTrk, driver, COUNT(finish) as Wins
-FROM nascarr AS race
-JOIN nascard AS driver
-   ON race.serRace = driver.serRace
-WHERE finish = 1 and roadTrk = 1
-GROUP BY roadTrk, driver
-ORDER BY Wins DESC
-
--- History of Jeff Gordon's wins on a road course
-
-Select race.year, track, roadTrk, driver, finish
-FROM nascarr AS race
-JOIN nascard AS driver
-   ON race.serRace = driver.serRace
-WHERE finish = 1 and roadTrk = 1 and driver = 'JeffGordon'
-ORDER BY race.year ASC
-
-
--- Determine which is a Road Course or Oval
-
-Select track, roadTrk,
+Select race.serRace, race.year, yrRacr, numCars, race.prize AS prize,
+race.laps, track, 
 CASE
    WHEN roadTrk = 1 THEN 'Road Course'
    ELSE 'Oval'
-END AS TrackType
-FROM nascarr
-GROUP BY track, roadTrk
-
--- Tracks: Road Courses
-
-Select track,
-CASE
-   WHEN roadTrk = 1 THEN 'Road Course'
-   ELSE 'Oval'
-END AS TrackType
-FROM nascarr
-WHERE roadTrk = 1
-GROUP BY track, roadTrk
-
--- Tracks: Oval
-
-Select track,
-CASE
-   WHEN roadTrk = 1 THEN 'Road Course'
-   ELSE 'Oval'
-END AS TrackType
-FROM nascarr
-WHERE roadTrk = 0
-GROUP BY track, roadTrk
-
--- Partition by total wins in the 1998 season and seeing the track type.
-
-Select race.serRace ,race.year, yrRacr, numCars, race.prize AS prize,
-race.laps, track, driver AS RaceWinner, carMake, COUNT(finish) OVER
-(PARTITION BY driver) as TotalWins,
-CASE
-   WHEN roadTrk = 1 THEN 'Road Course'
-   ELSE 'Oval'
-END AS TrackType
+END AS TrackType, driver AS RaceWinner, driver.prize AS WinningPrize, carMake
 FROM nascarr AS race
 JOIN nascard AS driver
    ON race.serRace = driver.serRace
-WHERE finish = 1 AND race.year = '1998'
+WHERE finish = 1 AND race.year = '1992'
 ORDER BY serRace
 
--- Number of races in different types of tracks
+-- Where did Bill Elliotts 5 wins come from?
 
-Select track, COUNT(track) as NumberOfRaces,
+Select race.serRace, race.year, yrRacr, numCars, race.prize AS prize,
+race.laps, track, 
 CASE
    WHEN roadTrk = 1 THEN 'Road Course'
    ELSE 'Oval'
-END AS TrackType
-FROM nascarr
-GROUP BY track, roadTrk
-ORDER BY COUNT(track) DESC
-
-SELECT TOP 10 driver, year, COUNT(finish) AS Top5s
-FROM nascard
-WHERE finish <= 5 AND year = '1998'
-GROUP BY driver, year
-ORDER BY Top5s DESC
-
--- Average Finish in 1998
-
-Select race.year, driver, COUNT(yearRace) as NumOfRaces, AVG(cast(finish as decimal)) as AvgFinish
+END AS TrackType, driver AS RaceWinner, driver.prize AS WinningPrize, carMake
 FROM nascarr AS race
 JOIN nascard AS driver
    ON race.serRace = driver.serRace
-Group by driver, race.year
-HAVING COUNT(yearRace) >= 27 AND race.year = '1998'
-ORDER BY AvgFinish
+WHERE finish = 1 AND race.year = '1992' AND driver = 'BillElliott'
+ORDER BY serRace
 
-
--- Create Temp table
-
-DROP Table if exists #Nascar
-Create Table #Nascar
-(
-Year numeric,
-YrRacr numeric,
-NumCars numeric,
-Prize numeric,
-Laps numeric,
-Track nvarchar(255),
-RaceWinner nvarchar(255),
-CarMake nvarchar(255),
-TotalWins numeric,
-TrackType nvarchar(255)
-)
-
-Insert into #Nascar
-Select race.year as Year, yrRacr, numCars, race.prize AS prize,
-race.laps, track, driver AS RaceWinner, carMake, COUNT(finish) OVER
-(PARTITION BY driver) as TotalWins,
-CASE
-   WHEN roadTrk = 1 THEN 'Road Course'
-   ELSE 'Oval'
-END AS TrackType
-FROM nascarr AS race
-JOIN nascard AS driver
-   ON race.serRace = driver.serRace
-WHERE finish = 1
-ORDER BY race.year, yearRace ASC
-
-Select *
-From #Nascar
-Order by Year, YrRacr
-
-Create View Nascar as
-Select race.year as Year, yrRacr, numCars, race.prize AS prize,
-race.laps, track, driver AS RaceWinner, carMake, COUNT(finish) OVER
-(PARTITION BY driver) as TotalWins,
-CASE
-   WHEN roadTrk = 1 THEN 'Road Course'
-   ELSE 'Oval'
-END AS TrackType
-FROM nascarr AS race
-JOIN nascard AS driver
-   ON race.serRace = driver.serRace
-WHERE finish = 1
-ORDER BY race.year, yearRace
